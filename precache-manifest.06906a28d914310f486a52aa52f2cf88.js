f728c6593149e6eff555e5cd3be4ea72589c332c
Carls13 <charlesshb98@gmail.com>
2019-01-26 01:14:39 -0400

    Updates

---
+++ precache-manifest.06906a28d914310f486a52aa52f2cf88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "cb5bff6f0a88cb5b3e9a",
+    "url": "/todo-list/static/js/main.cb5bff6f.chunk.js"
+  },
+  {
+    "revision": "3b23829783fa514e69ed",
+    "url": "/todo-list/static/js/1.3b238297.chunk.js"
+  },
+  {
+    "revision": "cb5bff6f0a88cb5b3e9a",
+    "url": "/todo-list/static/css/main.268d8c9f.chunk.css"
+  },
+  {
+    "revision": "bc0b31dcfac7cf4c76f96d9f5852511d",
+    "url": "/todo-list/index.html"
+  }
+];
